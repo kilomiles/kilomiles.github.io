7a2d0725d7eff649f2b5cc59d5f4cafb4d50f2bc
kilomiles <g2miles@cdf.toronto.edu>
2018-10-08 02:51:54 -0400

    redesign

--- js/scripts.js
+++ js/scripts.js
@@ -2,29 +2,28 @@ $(function () {
 	$("[data-toggle='tooltip']").tooltip({  })
 })
 
-function setClipboard (element, value, type) { 
-	var copy = $(element);
-	copy.tooltip("hide");
-	var oldValue = copy.html();
-	copy.html("&#10003; copied " + type + " to clipboard");
-	copy.css({
+function setClipboard (clickedElement, fieldValue, contentType) { 
+	var clicked = $(clickedElement);
+	clicked.tooltip("hide");
+	var oldValue = clicked.html();
+	clicked.html("&#10003; copied " + contentType + " to clipboard");
+	clicked.css({
 		"color": "limegreen"
 	});
 
 	var tempInput = document.createElement("input");
 	tempInput.style = "position: absolute; left: -1000px; top: -1000px";
-	tempInput.value = value;
+	tempInput.value = fieldValue;
 	document.body.appendChild(tempInput);
 	tempInput.select();
 	document.execCommand("copy");
 	document.body.removeChild(tempInput);
 
 	setTimeout(function () {	
-		copy.html(oldValue);
-		copy.css({
+		clicked.html(oldValue);
+		clicked.css({
 			"color": "inherit",
 			"font-weight": 300
 		});
 	}, 1500);
-
 }
